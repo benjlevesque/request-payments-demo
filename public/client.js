92a4303f1e6f964774a9f67ec5bdd1bdcd965e13
Glitch (hello-express) <none>
2019-01-22 16:49:20 +0000

    🍞👼 Checkpoint ./public/client.js:2253437/506

--- public/client.js
+++ public/client.js
@@ -7,18 +7,9 @@ const getData  = ()=> {
 
 const SimplePayment = SimplePaymentModule.default;
 const payment = new SimplePayment();
-payment.init("#request-payment");
-/*
-payment.init("#request-payment", ()=> {
-  const amount = document.getElementById("amount").val();
-  return {
-    amount,
-    currency: "EUR"
-  };
-});
 
-payment.init("#request-payment, {
-    amount: 100,
-    currency: "EUR"
-});*/
+payment.init("#request-payment"/*,  {
+  baseAmount: 70,
+  baseCurrency: "EUR"
+}*/);
 
