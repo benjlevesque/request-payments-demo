7c259d98fbc34c508d7e91179e1e0ed1cef6d162
Glitch (request-payments-demo) <none>
2019-01-23 21:23:33 +0000

    💾🎐 Updated with Glitch

--- public/client.js
+++ public/client.js
@@ -1,37 +1,13 @@
-// NOK
-const getData  = ()=> {
-  return {
-    amount: 110,
-    currency: "EUR"
-  };
-}
-
 // OK~
 const SimplePayment = SimplePaymentModule.default;
 const payment = new SimplePayment();
 
-// NOK
-payment.init("#request-payment");
 
 
+payment.init("#request-payment",'/request', () => ({
+  allowedPaymentCurrencies: ["ETH"],
+  baseAmount: 7,
+  baseCurrency: "EUR"
+}),{ buttonVariant: "dark" });
 
-(async () => {
-  const rawResponse = await fetch('/request', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(
-    {
-      currency: 'ETH',
-      amount: "1001000000000000000",
-      data: {
-        hello: 'world'
-      }
-    })
-  });
-  const content = await rawResponse.json();
 
-  console.log(content);
-})();
