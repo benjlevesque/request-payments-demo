3db48b9c28bbcc89a84ecc6c1f3c8a00fcf42abf
Glitch (request-payments-demo) <none>
2019-01-25 09:54:36 +0000

    🐈👾 Checkpoint ./public/index.html:2253437/4 ./server.js:2253437/481 ./package.json:2253437/6

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const request = require("@requestnetwork/payments-server")(process.env.API_KEY, {
-        apiUrl: "https://baguette-signer.request.network/api"
-      });
+const request = require("@requestnetwork/payments-server")(process.env.API_KEY);
 
 
 const app = express();
