fecf118e393ce3cb42fe403f43016fe24c0513d4
Glitch (request-payments-demo) <none>
2019-02-07 13:29:51 +0000

    ❄️👨‍👨‍👦 Updated with Glitch

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
-const request = require("@requestnetwork/payments-server")(process.env.API_KEY, {
-        apiUrl: "https://baguette-signer.request.network/api"
-      });
+const request = require("@requestnetwork/payments-server")(process.env.API_KEY);
 
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
-app.get("*.woff2", (req, res) => {
-  res.redirect(
-    "https://cdn.glitch.com/d8b7821c-1897-435c-81c4-78fdce354672%2F478ee99fcfc90f4f.woff2"
-  );
-});
 
     
 app.post("/request", request.handler(process.env.ETH_ADDRESS));
